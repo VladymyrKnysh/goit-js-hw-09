1a549fcac2d1188a07265202912c7d20ab776138
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-25 14:49:17 +0300

    task 9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+let timerId = null;
+
+
+startBtn.addEventListener("click", onStartBtnClick);
+stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+    startBtn.removeEventListener('click', onStartBtnClick)
+    timerId = setInterval(() => {
+      function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      }
+        const color = getRandomHexColor()
+    document.body.style.backgroundColor =  color  
+    }, 1000); 
+}
+   
+
+function onStopBtnClick() {
+    startBtn.addEventListener('click', onStartBtnClick)
+  clearInterval(timerId);
+}
+
+  
+
+
+
+
+
 
