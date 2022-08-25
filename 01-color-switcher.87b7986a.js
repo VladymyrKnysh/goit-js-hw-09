411e5705744fdc5a7d2eae7680572bc489ceb540
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-25 11:49:48 +0000

    Deploying to gh-pages from @ VladymyrKnysh/goit-js-hw-09@1a549fcac2d1188a07265202912c7d20ab776138 🚀

---
+++ 01-color-switcher.87b7986a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;function o(){t.removeEventListener("click",o),n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)}t.addEventListener("click",o),e.addEventListener("click",(function(){t.addEventListener("click",o),clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.87b7986a.js.map
